7a176533d6f2bab4bbee00d19a5160eada56a6f2
Hideo Hattori <hhatto.jp@gmail.com>
2021-05-18 10:15:27 +0900

    use alchemy for production

--- utils/web3.js
+++ utils/web3.js
@@ -1,3 +1,2 @@
-// TODO: temporary value
 export const HTTP_PROVIDER_URL =
-  'https://mainnet.infura.io/v3/f7a4c86a99304a2993a35bf53017fa38'
+  'https://eth-mainnet.alchemyapi.io/v2/yCkNjOGthDPmbEBs7H2hLAiUuudtRiad'
