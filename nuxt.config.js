a31d7f349c8ac975c249119887643b9011788fe7
aggre <hiroyuki.aggre@gmail.com>
2021-05-28 10:08:32 +0900

    add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,26 @@ export default {
         content: 'https://airdrop.devprotocol.xyz/image/eyecatch.png',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/favicon-96x96.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
