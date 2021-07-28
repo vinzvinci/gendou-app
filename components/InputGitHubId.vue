395457c15cd8980a836c646a4cbaed7092186246
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2021-07-28 17:13:42 +0900

    Entry period has expired. (#111)

--- components/InputGitHubId.vue
+++ components/InputGitHubId.vue
@@ -1,75 +1,11 @@
 <template>
-  <a-form-model
-    ref="inputForm"
-    :model="form"
-    :rules="rules"
-    @submit.prevent="submit"
-  >
-    <p>Enter your GitHub username to see your claimable coin :)</p>
-    <a-form-model-item class="model" prop="githubId">
-      <a-input
-        v-model="form.githubId"
-        type="text"
-        class="input display-5"
-        placeholder="GitHub username"
-      >
-      </a-input>
-    </a-form-model-item>
-  </a-form-model>
+  <p style="text-shadow: 2px 2px 4px rgba(255, 255, 255, 0.6)">
+    The entry period has expired.<br />
+    We are currently reviewing entries. See "What is DEV AIRDROP?" For future
+    schedules.<br />
+    Thank you for your application
+  </p>
 </template>
-
-<script>
-import Vue from 'vue'
-import { mapActions } from 'vuex'
-
-export default Vue.extend({
-  data() {
-    return {
-      form: { githubId: '' },
-      rules: {
-        githubId: [
-          {
-            required: true,
-            message: 'Please input your GitHub username',
-            trigger: 'change',
-          },
-        ],
-      },
-    }
-  },
-  beforeCreate() {
-    this.form = this.$form.createForm(this, { name: 'githubid-input' })
-  },
-  methods: {
-    submit() {
-      this.$refs.inputForm.validate(async (valid) => {
-        if (!valid) {
-          return
-        }
-
-        const key = 'fetch-info'
-        this.$message.loading({
-          content: 'fetch data...',
-          key,
-          duration: 0,
-        })
-
-        await this.getPrizeInfo(this.form.githubId)
-
-        this.$message.success({
-          content: 'Success',
-          key,
-          duration: 1,
-        })
-
-        // emit door open
-        this.$emit('startCallback')
-      })
-    },
-    ...mapActions(['getPrizeInfo']),
-  },
-})
-</script>
 
 <style lang="scss" scoped>
 p {
